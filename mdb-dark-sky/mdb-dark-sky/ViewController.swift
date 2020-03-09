7e5868605399a328cef1fb29da5bf68c363c35ed
James <jameswsj@berkeley.edu>
2020-03-08 21:53:17 -0700

    adding in starter files

---
+++ mdb-dark-sky/mdb-dark-sky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mdb-dark-sky
+//
+//  Created by James Jung on 3/8/20.
+//  Copyright © 2020 James Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
